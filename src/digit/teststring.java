a00d3ef832a7d96c94622551039a3f5bc9a77c41
dyxhhxx <15896883188@163.com>
2021-09-02 16:28:11 +0800

    test

--- src/digit/teststring.java
+++ src/digit/teststring.java
@@ -1,8 +1,11 @@
 package digit;
 
+import java.util.Locale;
+
 public class teststring {
     public static void main(String[] args) {
 
+        //生成字符串
         //方法1
         String garen="盖伦";
         garen="亚瑟";
@@ -16,97 +19,39 @@ public class teststring {
         System.out.println(Hero+"\t"+Hero1);
 
         //String类是被final修饰的，无法被继承，会报错
-//        static class MyString extends String{
-//
-//        }
-
-        //练习1：创建一个长度是5的随机字符串，随机字符有可能是数字，大写字母或者小写字母
-        char[] Sum=new char[62];
-        //添加数字0-9
-        for(int i1=48;i1<58;i1++){
-            int Num=i1;
-            char Char=(char)Num;  //ASCII码转换
-            Sum[i1-48]=Char;
-        }
-        //添加小写字母a-z
-        for(int i2=97;i2<123;i2++){
-            int Num=i2;
-            char Char=(char)Num;
-            Sum[i2-87]=Char;
-        }
-        //添加大写字母A-Z
-        for(int i2=65;i2<91;i2++){
-            int Num=i2;
-            char Char=(char)Num;
-            Sum[i2-29]=Char;
-        }
-        //打印出字符数组Sum检查是否完整
-        for(int i=0;i<62;i++){
-            System.out.println(Sum[i]);
-        }
-        //生成随机数组
-        char[] Random=new char[5];
-        for(int i=0;i<5;i++){
-            int ran=(int)(Math.random()*62);   //虽然Math.random()正好取到61
-            Random[i]=Sum[ran];
-        }
-        String StringRandom=new String(Random);
-        System.out.println(StringRandom);
-
-        //简化方法1：
-        char[] Char1=new char[5];
-        int startnum='0';
-        int endnum='z';
-        for(int i=0;i<Char1.length;i++){
-            //用一个while(true)的循环，将非数字或字母的字符剔除
-            while(true){
-                char e=(char)((int)(Math.random()*(endnum-startnum+1))+startnum);
-                if(Character.isLetter(e)||Character.isDigit(e)){
-                    Char1[i]=e;
-                    break;
-                }
-            }
-        }
-        System.out.println(new String(Char1));
-
-        //简化方法1(直接拼接字符串)
-        String Str1=new String("");
-        for(int i=0;i<5;i++){
-            while(true){
-                char e=(char)((int)(Math.random()*('z'-'0'+1))+'0');
-                if(Character.isLetter(e)||Character.isDigit(e)){
-                    Str1+=e;
-                    break;
-                }
-            }
-        }
-        System.out.println(Str1);
-
-        //简化方法2
-        //先把所有可能的字符挑选并集合在一起
-        String StrP="";
-        for(int i='0';i<='9';i++){
-            StrP+=(char)i;
-        }
-        for(int i='a';i<='z';i++){
-            StrP+=(char)i;
-        }for(int i='A';i<='Z';i++){
-            StrP+=(char)i;
-        }
-        System.out.println(StrP);
-        //从StrP中随机挑选5个字符出来
-        char[] Char2=new char[5];
-        for(int i=0;i<Char2.length;i++){
-            Char2[i]=StrP.charAt((int)(Math.random()*StrP.length()));
-        }
-        System.out.println(new String(Char2));
+//        static class MyString extends String{}
 
 
-
-
-
-
-
+        //操纵字符串
+        String sentence=" 盖伦，在进行了连续的8次击杀后，获得了 超神 的称号 ";
+        System.out.println(sentence);
+        //charAt(int index)获得指定位置的字符
+        System.out.println(sentence.charAt(3));
+        //toCharArray()获得对应的字符数组
+        char[] cs=sentence.toCharArray();
+        System.out.println(sentence.length()==cs.length);
+        //substring(start,end)截取子字符串
+        System.out.println(sentence.substring(2,5));
+        //split根据分隔符进行分隔
+        String[] sentences=sentence.split(",");
+        //trim去掉首尾空格
+        System.out.println(sentence.trim());
+        //toLowerCase全部变成小写
+        System.out.println(sentence.toLowerCase());
+        //toUpperCase全部变成大写
+        System.out.println(sentence.toUpperCase());
+        //indexOf判断字符或者子字符串出现的位置
+        System.out.println(sentence.indexOf('8'));
+        System.out.println(sentence.indexOf("超神"));
+        System.out.println(sentence.lastIndexOf('了'));
+        System.out.println(sentence.indexOf(',',5));
+        //contains是否包含子字符串
+        System.out.println(sentence.contains("击杀"));
+        //replaceAll替换所有的
+        System.out.println(sentence.replaceAll("击杀","被击杀"));
+        System.out.println(sentence.replace("超神","超鬼"));
+        //replaceFirst只替换第一个
+        System.out.println(sentence.replaceFirst(",","."));
 
     }
 }
