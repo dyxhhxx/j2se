8d7dac56bea7b2ed4e0986b0b763bad0c186e3aa
dyxhhxx <15896883188@163.com>
2021-09-02 16:06:48 +0800

    test

--- src/digit/teststring2.java
+++ src/digit/teststring2.java
@@ -27,35 +27,59 @@ public class teststring2 {
         String password=teststring2.randomstring(3);
 //        System.out.println("密码是："+password);
         //多层for循环
-        char[] guesspassword=new char[3];
-        outloop:
+
+//        char[] guesspassword=new char[3];
+//        outloop:
+//        for(int i='0';i<='z';i++){
+//            for(int j='0';j<='z';j++){
+//                for(int k='0';k<='z';k++){
+//                    if(Character.isLetterOrDigit((char)i)&&Character.isLetterOrDigit((char)j)&&Character.isLetterOrDigit((char)k)){
+//                        guesspassword[0]=(char)i;
+//                        guesspassword[1]=(char)j;
+//                        guesspassword[2]=(char)k;
+//                        String guess=new String(guesspassword);
+//                        System.out.println("尝试密码是："+guess);
+//                        if(guess.equals(password)){
+//                            System.out.println("密码破解成功！其为："+guess);
+//                            break outloop;
+//                        }
+//                    }
+//                    else{
+//                        continue;
+//                    }
+//                }
+//            }
+//        }
+//        System.out.println("密码是："+password);
+
+        //递归方法
+        char[] pwd1=new char[password.length()];
+        foundpwd(password,pwd1);
+        System.out.println("原密码是："+password);
+
+
+    }
+
+    public static void foundpwd(String pwd,char[] pwd1){
+        foundpwd(pwd,pwd1,0);
+    }
+
+    public static void foundpwd(String pwd,char[] pwd1,int index){
         for(int i='0';i<='z';i++){
-            for(int j='0';j<='z';j++){
-                for(int k='0';k<='z';k++){
-                    if(Character.isLetterOrDigit((char)i)&&Character.isLetterOrDigit((char)j)&&Character.isLetterOrDigit((char)k)){
-                        guesspassword[0]=(char)i;
-                        guesspassword[1]=(char)j;
-                        guesspassword[2]=(char)k;
-                        String guess=new String(guesspassword);
-                        System.out.println("尝试密码是："+guess);
-                        if(guess.equals(password)){
-                            System.out.println("密码破解成功！其为："+guess);
-                            break outloop;
-                        }
-                    }
-                    else{
-                        continue;
+            if(Character.isLetterOrDigit((char)i)){
+                char c=(char)i;
+                pwd1[index]=c;
+                if(index!=pwd.length()-1){
+                    foundpwd(pwd,pwd1,index+1);
+                }
+                else{
+                    String pwd11=new String(pwd1);
+                    if(pwd11.equals(pwd)){
+                        System.out.println("找到密码，其为："+pwd11);
                     }
                 }
             }
         }
-        System.out.println("密码是："+password);
-
-        //递归方法
-
     }
-    public static void foundpassword(){
 
-
-    }
 }
