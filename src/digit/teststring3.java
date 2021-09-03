b0366e067789f9041ab631d034f87753ee592eb5
dyxhhxx <15896883188@163.com>
2021-09-03 10:20:27 +0800

    test

--- src/digit/teststring3.java
+++ src/digit/teststring3.java
@@ -2,6 +2,24 @@ package digit;
 
 public class teststring3 {
 
+    //反转字符串数组
+    private static String[] reverseString(String[] oristr){
+        String[] relstr=new String[oristr.length];
+        for(int i=0;i<relstr.length;i++){
+            relstr[i]=oristr[oristr.length-i-1];
+        }
+        return relstr;
+    }
+
+    //将字符串数组转换为字符串（用" "连接）
+    private static String exstrarray(String[] strarray){
+        String result="";
+        for(int i=0;i<strarray.length;i++){
+            result+=strarray[i]+" ";
+        }
+        return(result.trim());
+    }
+
 
 
     public static void main(String[] args) {
@@ -58,28 +76,36 @@ public class teststring3 {
         //把最后一个two单词首字母大写
         String body="two eyes, two feet, one neck, two hands";
         String[] temp5=body.split(" ");
-        String[] temp6=new String[temp5.length];
-        String body1="";
-        for(int i=0;i<temp5.length;i++){
-            temp6[temp5.length-1-i]=temp5[i];
-        }
-        for(int i=0;i<temp6.length;i++){
-            body1+=temp6[i]+" ";
-        }
+        String[] temp6=reverseString(temp5);
+        String body1=exstrarray(temp6);
         String body2=body1.replaceFirst("two","Two");
         String[] temp7=body2.split(" ");
-        String[] temp8=new String[temp5.length];
-        String body3="";
-        for(int i=0;i<temp5.length;i++){
-            temp8[temp7.length-1-i]=temp5[i];
-        }
-        for(int i=0;i<temp6.length;i++){
-            body3+=temp8[i]+" ";
-        }
-        System.out.println(body3);
+        String[] temp8=reverseString(temp7);
+        String body3=exstrarray(temp8);
+        System.out.println(body+"\n"+body3);
 
 
-
-
+//        String[] temp6=new String[temp5.length];
+//        String body1="";
+//        for(int i=0;i<temp5.length;i++){
+//            temp6[temp5.length-1-i]=temp5[i];
+//        }
+//        for(int i=0;i<temp6.length;i++){
+//            body1+=temp6[i]+" ";
+//        }
+//        String body2=body1.replaceFirst("two","Two");
+//        System.out.println("body:"+body);
+//        System.out.println("body1:"+body1);
+//        System.out.println("body2:"+body2);
+//        String[] temp7=body2.split(" ");
+//        String[] temp8=new String[temp5.length];
+//        String body3="";
+//        for(int i=0;i<temp5.length;i++){
+//            temp8[temp7.length-1-i]=temp7[i];
+//        }
+//        for(int i=0;i<temp6.length;i++){
+//            body3+=temp8[i]+" ";
+//        }
+//        System.out.println("body3:"+body3);
     }
 }
